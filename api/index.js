d1d6d573b6666b53ff2eb5f5b2d79ef5968cbb66
gehcosta <geovannehc@gmail.com>
2024-12-20 14:50:35 -0300

    index.js mudança de porta

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const cookieParser = require('cookie-parser');
 const app = express();
-const port = 3000;
+const port = 5000;
 
 // Conexão com o banco de dados MySQL
 const db = mysql.createConnection({
